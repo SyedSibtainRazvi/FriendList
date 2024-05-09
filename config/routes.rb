54bcbafaf7513243aaa86972a410a4d0b27b32f8
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-05-09 14:08:48 +0530

    chore: added migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :friends
+  # Create all the crud routes for the friends controller
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get  'home/about'
